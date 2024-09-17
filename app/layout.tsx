337dfc5f863dd3ab477f8b83953571697c1e6acb
Karume-lab <danielkarume@outlook.com>
2024-09-17 18:07:45 +0300

    fix: :adhesive_bandage: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,22 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Platven LTD",
-  description: "Real estate platform",
+  title: 'Platven LTD',
+  description: 'Real estate platform',
+  openGraph: {
+    title: 'Platven LTD',
+    description: 'Explore real estate opportunities',
+    images: [
+      {
+        url: '/app/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Platven Real Estate',
+      },
+    ],
+    type: 'website',
+    siteName: 'Platven',
+  },
 };
 
 export default async function RootLayout({
