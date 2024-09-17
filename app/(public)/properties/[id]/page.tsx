c8854d75c23a39c29d2b246a90042bc195c2d9a3
Karume-lab <danielkarume@outlook.com>
2024-09-17 18:17:19 +0300

    fix: :lipstick: fix

--- app/(public)/properties/[id]/page.tsx
+++ app/(public)/properties/[id]/page.tsx
@@ -109,7 +109,7 @@ const PropertyDetailPage: FC<PropsWithPathParams> = async ({
                     <Heart />
                   </Button>
                   <Button>
-                    <ShareProperty propertyUrl={`${siteConfig.url}/properties/${property.id}`} title={`View this property on ${siteConfig.url}`} />
+                    <ShareProperty propertyUrl={`${siteConfig.url}/properties/${property.id}`} title={`View this property on ${siteConfig.name}`} />
                   </Button>
                 </div>
               </div>
